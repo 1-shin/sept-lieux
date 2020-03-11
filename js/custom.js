936aaee18648acd599055386a5e8d46b7332cc60
Daisuke Nashiro <nashirox@gmail.com>
2020-03-11 21:40:40 +0900

    Update site

---
+++ js/custom.js
@@ -0,0 +1,57 @@
+(function ($) {
+	
+	$(window).load(function(){
+      $("#navigation").sticky({ topSpacing: 0 });
+    });
+
+	jQuery(window).load(function() { 
+		jQuery("#preloader").delay(100).fadeOut("slow");
+		jQuery("#load").delay(100).fadeOut("slow");
+	});
+
+
+	//jQuery for page scrolling feature - requires jQuery Easing plugin
+	$(function() {
+		$('.navbar-nav li a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+		$('.page-scroll a').bind('click', function(event) {
+			var $anchor = $(this);
+			$('html, body').stop().animate({
+				scrollTop: $($anchor.attr('href')).offset().top
+			}, 1500, 'easeInOutExpo');
+			event.preventDefault();
+		});
+	});
+	
+	//owl carousel
+	$('#owl-works').owlCarousel({
+            items : 4,
+            itemsDesktop : [1199,5],
+            itemsDesktopSmall : [980,5],
+            itemsTablet: [768,5],
+            itemsTabletSmall: [550,2],
+            itemsMobile : [480,2],
+        });
+		
+	$('.item').tile();
+
+	//parallax
+        if ($('.parallax').length)
+        {
+			$(window).stellar({
+				responsive:true,
+                scrollProperty: 'scroll',
+                parallaxElements: false,
+                horizontalScrolling: false,
+                horizontalOffset: 0,
+                verticalOffset: 0
+            });
+
+        }
+
+})(jQuery);
